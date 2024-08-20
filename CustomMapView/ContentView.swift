dc8d03c12cf977904bcd39778737705bd817fa68
Elkin Garcia <elkingarcia.11@gmail.com>
2024-08-20 17:49:14 -0400

    Initial Commit

---
+++ CustomMapView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomMapView
+//
+//  Created by Elkin Garcia on 8/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomMapView/CustomMapViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomMapViewApp.swift
+//  CustomMapView
+//
+//  Created by Elkin Garcia on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomMapViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
